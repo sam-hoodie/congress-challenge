6fbf79d0d4bdd7bced29e4d28c22d7f0d3774ffc
Sam G <112674910+sam-hoodie@users.noreply.github.com>
2022-09-09 19:33:07 -0400

    Initial import of congress data
    From https://github.com/unitedstates/congress-legislators under CC0 license

---
+++ src/main/kotlin/Congress.kt
@@ -0,0 +1,60 @@
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.type.TypeFactory
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
+
+data class Person(
+    val name: Name,
+    val bio: Bio,
+    val terms: List<Term>,
+)
+
+data class Name(
+    val first: String,
+    val last: String,
+)
+
+data class Bio(
+    val birthday: String,
+    val gender: String,
+)
+
+data class Term(
+    val type: String,
+    val start: String,
+    val end: String,
+    val state: String,
+    val party: String,
+)
+
+fun parse(): List<Person> {
+    val input = Person::class.java.getResourceAsStream("/legislators-current.yaml")
+    val mapper = ObjectMapper(YAMLFactory())
+    mapper.registerModule(KotlinModule())
+    mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+
+    val typeFactory: TypeFactory = mapper.typeFactory
+
+    return try {
+        mapper.readValue(input, typeFactory.constructCollectionType(
+            MutableList::class.java,
+            Person::class.java
+        ))
+    } catch (exception: MissingKotlinParameterException) {
+        println("Could not read YAML file!")
+        println(exception.message)
+        throw exception
+    }
+}
+
+fun main() {
+    val result = parse()
+
+    println(result.size)
+    for (person in result) {
+        println("${person.name.first} ${person.name.last} served ${person.terms.size} time(s)")
+    }
+}

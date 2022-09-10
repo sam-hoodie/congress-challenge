5cde5db6f4fae57acb28471e184450a71a2ae949
Sam G <112674910+sam-hoodie@users.noreply.github.com>
2022-09-10 19:24:54 -0400

    Give directions for age and gender commands

--- src/main/kotlin/GiveDirections.kt
+++ src/main/kotlin/GiveDirections.kt
@@ -5,11 +5,15 @@ fun main() {
 fun printDirections() {
     println("To get the directions to commands for these genres, type: ")
     println("  0: Names Information")
+    println("  1: Age and Gender Info")
     print("    > ")
 
     val directionInput = readLine().toString().toInt()
     if (directionInput == 0) {
         printDirectionsNames()
+    }
+    if (directionInput == 1) {
+        printAgeAndGenderDirections()
     }
  }
 
@@ -20,10 +24,25 @@ fun printDirectionsNames() {
     println("      2: Get longest first name")
     println("      3: Get longest last name")
     print("        > ")
-    printDirectionsNames()
     val subinput = readLine().toString().toInt()
-    if (subinput == 0) { println("        Type the command: get congress.names.shortestFirst") }
-    if (subinput == 1) { println("        Type the command: get congress.names.longestFirst") }
-    if (subinput == 2) { println("        Type the command: get congress.names.shortestLast") }
-    if (subinput == 3) { println("        Type the command: get congress.names.longestLast") }
+    if (subinput == 0) { println("        Type the command: get congress.names.shortestFirst"); return }
+    if (subinput == 1) { println("        Type the command: get congress.names.longestFirst"); return }
+    if (subinput == 2) { println("        Type the command: get congress.names.shortestLast"); return }
+    if (subinput == 3) { println("      >> Type the command: get congress.names.longestLast <<"); return }
+    println("          Invalid input")
+}
+
+fun printAgeAndGenderDirections() {
+    println("      To get the age/gender commands, type: ")
+    println("      0: Get the youngest congressman")
+    println("      1: Get the oldest congressman")
+    println("      2: Get the average age of the congressmen")
+    println("      3: get the most common gender of the congressmen")
+    print("        > ")
+    val subinput = readLine().toString().toInt()
+    if (subinput == 0) { println("        Type the command: get congress.age.youngest"); return }
+    if (subinput == 1) { println("        Type the command: get congress.age.oldest"); return }
+    if (subinput == 2) { println("        Type the command: get congress.age.average"); return }
+    if (subinput == 3) { println("      >> Type the command: get congress.gender.prevalent <<"); return }
+    println("          Invalid input")
 }

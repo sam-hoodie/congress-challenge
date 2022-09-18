540afb91d44fe06f0eb9d7612e095b5ed007b5d6
Sam G <112674910+sam-hoodie@users.noreply.github.com>
2022-09-18 19:45:12 -0400

    Enable running in terminal
    Run `.\gradlew run`

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    application
 }
 
 group = "org.example"
@@ -18,4 +19,12 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("GetInputKt")
+}
+
+tasks.withType<JavaExec>() {
+    standardInput = System.`in`
 }
